ff687a6dbf9deee9c13e355e0e721e1fad42d7d9
Marco Mello <marmello@gmail.com>
2018-11-14 10:37:16 -0200

    Update network-drawing.R

--- network-drawing.R
+++ network-drawing.R
@@ -9,24 +9,12 @@
 ##### Author: Marco Mello
 ##### E-mail: marmello@gmail.com 
 ##### Script: Script for drawing networks in R
-##### How to cite: Mello MAR. 2017. Script for drawing networks in R. Available
-##### at https://marcomellolab.wordpress.com.
+##### How to cite: Mello MAR. 2017. Script for drawing networks in R. Available at https://marcomellolab.wordpress.com.
 ##### Published on April 25th, 2017 (English version).
 ##### Updated on November 22nd, 2017 (English version).
 ##### Run in R 3.3.3 (2017-03-06) -- "Another Canoe"
 
-##### Disclaimer: You may use this script freely for non-comercial
-##### purposes at your own risk. We assume no responsibility or
-##### liability for the use of this software, convey no license
-##### or title under any patent, copyright, or mask work right
-##### to the product. We reserve the right to make changes in
-##### the software without notification. 
-##### We also make no representation or warranty that such
-##### application will be suitable for the specified use without
-##### further testing or modification. If this script helps you
-##### produce any academic work (paper, book, chapter, 
-##### dissertation etc.), please acknowledge the authors and
-##### cite the source.
+##### Disclaimer: You may use this script freely for non-comercial purposes at your own risk. We assume no responsibility or liability for the use of this software, convey no license or title under any patent, copyright, or mask work right to the product. We reserve the right to make changes in the software without notification. We also make no representation or warranty that such application will be suitable for the specified use without further testing or modification. If this script helps you produce any academic work (paper, book, chapter, dissertation etc.), please acknowledge the authors and cite the source.
 
 
 #############################################################
